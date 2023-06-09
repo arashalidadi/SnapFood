23e5619da942fac6d1f053359ae230f817a6d4e9
Arash@m <alidadiarash71@gmail.com>
2023-06-10 04:24:03 +0430

    create ho,escreen UI,categories,categorycard,featuredrow component and there ui

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,80 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import { ScrollView } from "react-native";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View className="flex-1 items-center justify-center bg-lime-600">
-      <Text className="text-red-700">Home Screen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5 ">
+      {/* header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* search box */}
+      <View className="flex-row items-center pb-2 mx-4 ">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={35} />
+          <TextInput placeholder="Restaurants" keyboardType="default" />
+        </View>
+
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+        {/* fearured */}
+        <FeaturedRow
+          id="123"
+          title="Featured"
+          description="Paid placement from our partners"
+          // featuredCategory="featured"
+        />
+        {/* Tasty Discounts */}
+        <FeaturedRow
+          id="1234"
+          title="Tasty Discounts"
+          description="Everyone been enjoying these huicy discounts!"
+          // featuredCategory="discounts"
+        />
+        {/* Offers near you */}
+        <FeaturedRow
+          id="12345"
+          title="Offers near you"
+          description="why not support your local restaurant tonight!"
+          // featuredCategory="offers"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+// const nativewind = require("nativewind/tailwind/css");
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
+    "./App.{js,jsx,ts,tsx,html}",
+    "./index.{js,jsx,ts,tsx,html}",
     "./screens/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
