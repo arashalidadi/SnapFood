92a249660b8574aa7793a3b08fa22ed6adc94754
Arash@m <alidadiarash71@gmail.com>
2023-06-09 07:38:59 +0430

    ready and install packeges for snapfood project

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-lime-600">
+      <Text className="text-red-700">Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
